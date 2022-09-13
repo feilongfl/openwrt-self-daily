019e6aec8e1c37e251fea7e66191df287bd95913
YuLong Yao <feilongphone@gmail.com>
2022-09-13 12:40:03 +0800

    fake build

--- src/compile.sh
+++ src/compile.sh
@@ -8,7 +8,7 @@ if not test -f feeds.conf.backup
 cp feeds.conf.default feeds.conf.backup
 end
 cat feeds.conf.backup ../configs/feeds/feeds.conf.append > feeds.conf.default
-./scripts/feeds update -a && ./scripts/feeds install -a
+# ./scripts/feeds update -a && ./scripts/feeds install -a
 
 # set compile config
 cp ../configs/target/nanopi_r1.config .config
@@ -17,8 +17,19 @@ cp ../configs/target/nanopi_r1.config .config
 cp -rv ../configs/patches/platform/*.patch target/linux/sunxi/patches-5.10/
 
 # download
-make download -j(nproc)
-make -j(nproc)
+# make download -j(nproc)
+# make -j(nproc)
+mkdir -p bin/targets/sunxi/cortexa7
+pushd bin/targets/sunxi/cortexa7
+touch sha256sums
+touch profiles.json
+touch feeds.buildinfo
+touch config.buildinfo
+touch version.buildinfo
+touch feilongwrt-22.03-snapshot-unknown-sunxi-cortexa7-friendlyarm_nanopi-r1.manifest
+touch feilongwrt-22.03-snapshot-unknown-sunxi-cortexa7-friendlyarm_nanopi-r1-squashfs-sdcard.img.gz
+touch feilongwrt-22.03-snapshot-unknown-sunxi-cortexa7-friendlyarm_nanopi-r1-ext4-sdcard.img.gz
+popd
 
 # exit workspace
 popd
